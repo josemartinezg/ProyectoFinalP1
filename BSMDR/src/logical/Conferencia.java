2b00cdee782b82af6b4522ff2123ccd5036ccdd9
jmartinezg <jmlmartinezg@outlook.com>
2018-07-14 19:02:15 -0400

    Resolving conflicts

--- BSMDR/src/logical/Conferencia.java
+++ BSMDR/src/logical/Conferencia.java
@@ -1,42 +1,64 @@
 package logical;
 
+<<<<<<< HEAD
 import java.util.ArrayList;
 
 public class Conferencia {
-	private ArrayList<Equipo> misEquipos;
-	private ArrayList<Jugador> misJugadores;
-	private ArrayList<Juego> misJuegos;
-	private static Conferencia conferencia = null;
-
+	private ArrayList<Juego> juegos;
+	private ArrayList<Equipo> equipos;
+	private static Conferencia conf = null;
+	
 	private Conferencia() {
 		super();
-		misEquipos = new ArrayList<>();
-		misJugadores = new ArrayList<>();
-		misJuegos = new ArrayList<>();
+		this.juegos = new ArrayList<Juego>();
+		this.equipos = new ArrayList<Equipo>();
 	}
-	public static Conferencia getInsance() {
-		if (conferencia == null) {
-			conferencia = new Conferencia();
-		}
-		return conferencia;
-	}
-	public ArrayList<Equipo> getMisEquipos() {
-		return misEquipos;
-	}
-	public void setMisEquipos(ArrayList<Equipo> misEquipos) {
-		this.misEquipos = misEquipos;
-	}
-	public ArrayList<Jugador> getMisJugadores() {
-		return misJugadores;
-	}
-	public void setMisJugadores(ArrayList<Jugador> misJugadores) {
-		this.misJugadores = misJugadores;
-	}
-	public ArrayList<Juego> getMisJuegos() {
-		return misJuegos;
-	}
-	public void setMisJuegos(ArrayList<Juego> misJuegos) {
-		this.misJuegos = misJuegos;
+
+	public ArrayList<Juego> getJuegos() {
+		return juegos;
 	}
 	
-}
+	public ArrayList<Juego> getJuegos(FechaSimple fecha) {
+		ArrayList<Juego> aux = new ArrayList<Juego>();
+		for (Juego jg : juegos) {
+			FechaSimple fechaJuego = jg.getFecha();
+			if (fechaJuego.getMonth() == fecha.getMonth() &&
+				fechaJuego.getYear() == fecha.getYear() &&
+				fechaJuego.getDay() == fecha.getDay()) {
+				aux.add(jg);
+			}
+		}
+		return aux;
+	}
+
+	public ArrayList<Equipo> getEquipos() {
+		return equipos;
+	}
+	
+	public void insertJuego(Juego juego) {
+		this.juegos.add(juego);
+	}
+	
+	public void insertEquipo(Equipo equipo) {
+		this.equipos.add(equipo);
+	}
+	
+	public void removeJuego(Juego juego) {
+		this.juegos.remove(juego);
+	}
+	
+	public void removeEquipo(Equipo equipo) {
+		this.equipos.remove(equipo);
+	}
+	
+	public static Conferencia getInstance() {
+		if (conf == null) {
+			conf = new Conferencia();
+		}
+		return conf;
+	}
+	
+	public ArrayList<Juego> getJuegosDelDia() {
+		return this.getJuegos(new FechaSimple());
+	}
+
